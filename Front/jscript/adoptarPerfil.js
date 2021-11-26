48efc240088c826b3918bc4bb98a25edb301508b
Tomas Molino <59565479+totomolino@users.noreply.github.com>
2021-11-26 10:58:06 -0300

    Ya se notifica al duenio de adopcion

--- Front/jscript/adoptarPerfil.js
+++ Front/jscript/adoptarPerfil.js
@@ -21,7 +21,27 @@ var app = new Vue({
     //NOSE QUE MIERDA COPIE PERO HAY QUE LLAMAR A LA PUBLICACION ADOPCION PARA MOSTRAR LA INFO DE CADA PERFIL 
     // CUANDO EL ADOPTANTE APRETA EL BOTON ADOPTAR -> NOTIFICAR AL DUENIO 
     
-    methods: {        
+    methods: {          
+        adoptarMascota: function(){
+            const url = "http://localhost:4567/patitas/mascota/adoptar/" + this.idMascota
+            const req = {
+                "mascota": this.idMascota,
+                "adoptante": localStorage.getItem("IDPERSONA")
+            }
+            fetch(url,{
+                method: "POST",
+                body: JSON.stringify(req)
+            })
+            .then(Response => {
+                error(Response.status, "No se pudo adoptar a la mascota")
+                return Response.json()
+            })
+            .then(algo => {
+                alert(algo.mensaje)
+                document.getElementById("index").click();
+                //apretar algun boton xd
+            })
+        }      
     },
     created(){
         const urlParams = new URLSearchParams(window.location.search);
